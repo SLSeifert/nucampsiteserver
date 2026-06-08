db4f4a4e2d9897f675400d5f8d4982e320d7bb35
SLSeifert <slseifert1986@gmail.com>
2026-06-07 22:56:27 -0700

    Enable file uploading to server with Multer middleware.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const partnersRouter = require("./routes/partnersRouter");
 const promotionsRouter = require("./routes/promotionsRouter");
+const uploadRouter = require("./routes/uploadRouter");
 
 const mongoose = require("mongoose");
 
@@ -58,6 +59,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionsRouter);
 app.use("/partners", partnersRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
